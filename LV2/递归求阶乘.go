3b2875cac4446e368e7409ba4d01b22367209647
Ferdinadaedth <115557428+Ferdinadaedth@users.noreply.github.com>
2023-04-16 22:46:41 +0800

    Update 递归求阶乘.go

--- LV2/递归求阶乘.go
+++ LV2/递归求阶乘.go
@@ -1,1 +1,17 @@
+package main
+
+import "fmt"
+
+func multiple(n int) int {
+	if n == 0 {
+		return 1
+	} else {
+		return n * multiple(n-1)
+	}
+}
+func main() {
+	var a int
+	fmt.Scan(&a)
+	fmt.Println(multiple(a))
+}
 
